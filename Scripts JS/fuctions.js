0bfb05d3e553ea8a3b441a78d16a1688779f0e72
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-06-16 05:17:27 -0400

    Index completo, algunas funcionalidades en js

---
+++ Scripts JS/fuctions.js
@@ -0,0 +1,16 @@
+function mostrarLote2(){
+    document.getElementById('lote1').style.display='none';
+    document.getElementById('lote2').style.display='block';
+}
+function mostrarLote1(){
+    document.getElementById('lote2').style.display='none';
+    document.getElementById('lote1').style.display='block';
+}
+function mostrarLote4(){
+    document.getElementById('lote3').style.display='none';
+    document.getElementById('lote4').style.display='block';
+}
+function mostrarLote3(){
+    document.getElementById('lote4').style.display='none';
+    document.getElementById('lote3').style.display='block';
+}
