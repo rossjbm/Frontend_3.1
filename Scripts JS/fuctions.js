ee8c9ccbc914fd1f81d721d2f45a64453f21d3b5
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-06-17 04:13:14 -0400

    intento de conexion

--- Scripts JS/fuctions.js
+++ Scripts JS/fuctions.js
@@ -14,19 +14,3 @@ function mostrarLote3(){
     document.getElementById('lote4').style.display='none';
     document.getElementById('lote3').style.display='block';
 }
-
-const creacionResena = document.getElementById('crearResena');
-
-creacionResena.addEventListener('submit', (event) => {
-    event.preventDefault();
-    var libro = document.getElementById('libro').value
-    var autor_libro = document.getElementById('autor_libro').value
-    var fecha_culminacion = document.getElementById('fecha_culminacion').value
-    var calificacion = document.getElementById('calificacion').value
-    var resena = document.getElementById('resena').value
-
-    var cr = {libro: libro, autor_libro: autor_libro, fecha_culminacion:fecha_culminacion, calificacion:calificacion, resena:resena}
-    var crJSON = JSONstringify(cr)
-    console.log(crJSON)
-
-})
